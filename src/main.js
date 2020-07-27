e8f4847b79a1380ec856ab9434bb9b3d9641d2d8
snowzijun <1451965647@qq.com>
2020-07-27 19:00:05 +0800

    feat: 优化

--- src/main.js
+++ src/main.js
@@ -2,20 +2,15 @@
  * @Author: 子君
  * @Date: 2020-07-21 12:29:49
  * @LastEditors: 子君
- * @LastEditTime: 2020-07-21 13:20:38
+ * @LastEditTime: 2020-07-27 18:58:07
  * @Description: 文件说明
  * @FilePath: \vue-element-table\src\main.js
  */
-// 扫描所有的组件
 import ZjTable from './components/zj-table'
-
-const components = [ZjTable]
 
 export default {
   install(Vue) {
-    components.forEach(component => {
-      Vue.component(component.name, component)
-    })
+    Vue.component(ZjTable.name, ZjTable)
   },
   ZjTable
 }
